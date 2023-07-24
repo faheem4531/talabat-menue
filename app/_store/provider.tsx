080b1091a6a751ff8d74926cb7e6f6195b1955df
fahadtufail007 <fahadtufail873@gmail.com>
2023-07-24 18:04:18 +0500

    adds: translation provider in providers

--- app/_store/provider.tsx
+++ app/_store/provider.tsx
@@ -3,12 +3,17 @@
 import { persistor, store } from "./store";
 import { Provider } from "react-redux";
 import { PersistGate } from 'redux-persist/es/integration/react';
+import { I18nextProvider } from 'react-i18next';
+import i18n from '../../i18n';
 
 export function Providers({ children }: { children: React.ReactNode }) {
 
   return (
+    <I18nextProvider i18n={i18n}>
     <PersistGate persistor={persistor} loading={null}>
       <Provider store={store}>{children}</Provider>
     </PersistGate>
+    </I18nextProvider>
+
   );
 }
