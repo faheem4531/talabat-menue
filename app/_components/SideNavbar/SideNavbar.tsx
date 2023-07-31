fec1452f1a2913891113040b04463f1f814edf36
tufailfahad01 <tufailfahad01@gmail.com>
2023-07-31 16:23:17 +0500

    fix modals ui

--- app/_components/SideNavbar/SideNavbar.tsx
+++ app/_components/SideNavbar/SideNavbar.tsx
@@ -303,9 +303,8 @@ const SideNavbar: FC<SideNavbar> = ({ hamBurgerIcon }: SideNavbar) => {
             </li>
             <li onClick={() => setourGoasModalOpen(true)}>
               <div
-                className={`flex    ${
-                  lang === "ar" ? "flex-row-reverse gap-4" : "items-center"
-                }
+                className={`flex    ${lang === "ar" ? "flex-row-reverse gap-4" : "items-center"
+                  }
                  p-2 py-4 cursor-pointer text-gray-900 rounded-lg text-sm px-5 dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group`}
                 onClick={handleNavbarToggle}
               >
@@ -314,18 +313,15 @@ const SideNavbar: FC<SideNavbar> = ({ hamBurgerIcon }: SideNavbar) => {
                   aria-hidden="true"
                   xmlns="http://www.w3.org/2000/svg"
                   fill="currentColor"
-                  viewBox="0 0 20 20"
+                  viewBox="0 0 24 24"
                 >
-                  <path d="M5 5V.13a2.96 2.96 0 0 0-1.293.749L.879 3.707A2.96 2.96 0 0 0 .13 5H5Z" />
-                  <path d="M6.737 11.061a2.961 2.961 0 0 1 .81-1.515l6.117-6.116A4.839 4.839 0 0 1 16 2.141V2a1.97 1.97 0 0 0-1.933-2H7v5a2 2 0 0 1-2 2H0v11a1.969 1.969 0 0 0 1.933 2h12.134A1.97 1.97 0 0 0 16 18v-3.093l-1.546 1.546c-.413.413-.94.695-1.513.81l-3.4.679a2.947 2.947 0 0 1-1.85-.227 2.96 2.96 0 0 1-1.635-3.257l.681-3.397Z" />
-                  <path d="M8.961 16a.93.93 0 0 0 .189-.019l3.4-.679a.961.961 0 0 0 .49-.263l6.118-6.117a2.884 2.884 0 0 0-4.079-4.078l-6.117 6.117a.96.96 0 0 0-.263.491l-.679 3.4A.961.961 0 0 0 8.961 16Zm7.477-9.8a.958.958 0 0 1 .68-.281.961.961 0 0 1 .682 1.644l-.315.315-1.36-1.36.313-.318Zm-5.911 5.911 4.236-4.236 1.359 1.359-4.236 4.237-1.7.339.341-1.699Z" />
+                  <path fill="currentColor" d="M20.172 6.75h-1.861l-4.566 4.564a1.874 1.874 0 1 1-1.06-1.06l4.565-4.565V3.828a.94.94 0 0 1 .275-.664l1.73-1.73a.249.249 0 0 1 .25-.063c.089.026.155.1.173.191l.46 2.301l2.3.46c.09.018.164.084.19.173a.25.25 0 0 1-.062.249l-1.731 1.73a.937.937 0 0 1-.663.275Z" /><path fill="currentColor" d="M2.625 12A9.375 9.375 0 0 0 12 21.375A9.375 9.375 0 0 0 21.375 12c0-.898-.126-1.766-.361-2.587A.75.75 0 0 1 22.455 9c.274.954.42 1.96.42 3c0 6.006-4.869 10.875-10.875 10.875S1.125 18.006 1.125 12S5.994 1.125 12 1.125c1.015-.001 2.024.14 3 .419a.75.75 0 1 1-.413 1.442A9.39 9.39 0 0 0 12 2.625A9.375 9.375 0 0 0 2.625 12Z" /><path fill="currentColor" d="M7.125 12a4.874 4.874 0 1 0 9.717-.569a.748.748 0 0 1 1.047-.798c.251.112.42.351.442.625a6.373 6.373 0 0 1-10.836 5.253a6.376 6.376 0 0 1 5.236-10.844a.75.75 0 1 1-.17 1.49A4.876 4.876 0 0 0 7.125 12Z" />
                 </svg>
                 <span
-                  className={` ml-4 whitespace-nowrap ${
-                    lang === "ar" ? "" : "flex-1"
-                  }`}
+                  className={` ml-4 whitespace-nowrap ${lang === "ar" ? "" : "flex-1"
+                    }`}
                 >
-                  {t("sideBar.goals")}
+                  {t('sideBar.goals')}
                 </span>
               </div>
             </li>
